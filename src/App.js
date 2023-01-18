64667db00130c30183590a38b26b4818e2bd6e9f
Mohammed Zeyad Abu Raida <mohammedreda814@gmail.com>
2023-01-18 23:53:53 +0200

    Adding task in github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './App.css';
+/*
+Create a palette color:
 
-function App() {
+Create an array with a group of colors.
+Loop through this array and create a button for each color.
+Clicking the button will change the state color of the page
+you can add the style directly to the div
+    <div style={{backgroundColor: color}}>
+
+*/
+const App = () => {
+  const colors = [
+    "#7B8FA1",
+    "#567189",
+    "#E4C988",
+    "#C27664",
+    "#658864",
+    "#13005A",
+    "#820000",
+    "#6F1AB6",
+    "#F3CCFF",
+    "#227C70"
+  ];
+  const [backgroundColor, setBackgoundColor] = useState("#7B8FA1");
+  const [current, setCurrent] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: backgroundColor }}>
+      {current !== null && <h1>Copied {current}</h1>}
+      <div className='container'>
+        {colors.map((color, index) => (
+          <div className='card' key={index}>
+            <div
+              style={{
+                background: color,
+                filter: "brighness(85%)",
+                boxShadow: color === backgroundColor ? "0 0 5px #000" : ""
+              }}
+              className='box'
+              onClick={() => setBackgoundColor(color)}>
+            </div>
+
+            <CopyToClipboard text={`color ${color}:`}>
+              <p
+                style={{
+                  color: color === backgroundColor ? "#000" : color
+                }}
+                onClick={() => setCurrent(color)}
+              >
+                {color}
+              </p>
+            </CopyToClipboard>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
